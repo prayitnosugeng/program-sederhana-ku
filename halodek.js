0d94d3b9217d55f505fbe6ffedf6065f81d36dc5
prayitnosugeng <106417531+prayitnosugeng@users.noreply.github.com>
2022-12-17 20:42:06 +0700

    Add files via upload

---
+++ halodek.js
@@ -0,0 +1,14 @@
+
+var jeneng = prompt('tulis nama kamu')
+alert('Have a great day ' + jeneng +' 😊');
+var pacar = confirm(jeneng + ' kira2 mau gk jd pcrku ??');
+
+if (pacar == true) {
+    
+} else {
+    while (! pacar == true) {
+        pacar = confirm (jeneng + ' mau ngga jadi pacarku??');
+    }
+}
+alert('Hore!!! Kita resmi pacaran wkwkwk');
+
